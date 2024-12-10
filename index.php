8fa5d9992c4a8c390639a5296277cce1760fd8f8
Ricardo <ricarditomontserrat@gmail.com>
2024-12-10 01:02:16 +0100

    Added globals, logging database, start order and finished mvp setup

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
 
+$ROUTES = __DIR__ . '/routes/';
+$CACHE = __DIR__ . '/cache/';
+$HTML = __DIR__ . '/html/';
+$CSS = __DIR__ . '/css/';
+
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes_dir = __DIR__ . '/routes/';
-$routes = ['/' => $routes_dir . 'index.php'];
+$routes = ['/' => $ROUTES . 'index.php'];
 
-foreach (scandir($routes_dir) as $file) {
+foreach (scandir($ROUTES) as $file) {
     if ($file === '.' || $file === '..') continue;
 
     $route = substr($file, 0, -4);
-    $routes["/{$route}"] = $routes_dir . $file;
+    $routes["/{$route}"] = $ROUTES . $file;
 }
 
 if (!array_key_exists($uri, $routes)) {
--- routes/index.php
+++ routes/index.php
@@ -1,7 +1,9 @@
 <?php
 
-$theme = 'blue';
+$db = new SQLite3($CACHE . 'db.sqlite', SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
+$db->exec("CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, log_time CURRENT_TIMESTAMP, session_time DATETIME);");
 
+$theme = 'blue';
 session_start();
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
@@ -10,9 +12,15 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
     if ($user == 'admin' && $pass == 'admin') {
         $_SESSION['auth'] = true;
+        $_SESSION['session'] = date('Y-m-d H:i:s');
+
+        $db->exec("INSERT INTO logs (ip, log_time) VALUES ('{$_SERVER['REMOTE_ADDR']}', '{$_SESSION['session']}');") or die($db->lastErrorMsg());
+        $_SESSION['id'] = $db->lastInsertRowID();
+
         $message .= "<span hx-get='/' hx-target='body' hx-trigger='every 0.5s'>Success!</span>";
         $color = 'green';
     } else {
+        $db->exec("INSERT INTO logs (ip, log_time) VALUES ('{$_SERVER['REMOTE_ADDR']}', '{$_SESSION['session']}');") or die($db->lastErrorMsg());
         http_response_code(401);
         $message .= "Wrong username or password";
         $color = 'red';
@@ -24,8 +32,20 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
     die($html);
 } elseif (isset($_POST['logout'])) {
+    $duration = time() - strtotime($_SESSION['session']);
+    $db->exec("UPDATE logs SET session_time = '{$duration}' WHERE id = {$_SESSION['id']}") or die($db->lastErrorMsg());
+
     session_destroy();
     header('Location: /');
 }
 
-view('index', ['auth' => $_SESSION['auth'], 'theme' => $theme]);
+$query = $db->query('SELECT * FROM logs ORDER BY id DESC LIMIT 10') or die($db->lastErrorMsg());
+
+$logs = [];
+while ($row = $query->fetchArray(SQLITE3_ASSOC)) {
+    $logs[] = $row;
+}
+
+$db->close();
+
+view('index', ['auth' => $_SESSION['auth'], 'theme' => $theme, 'logs' => $logs]);
