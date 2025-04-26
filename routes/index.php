01bf4cbae0b6b1b78169eed5ac30c8748c31fff8
Ricardo <ricarditomontserrat@gmail.com>
2025-04-26 04:01:47 +0200

    Added some query images

--- routes/index.php
+++ routes/index.php
@@ -3,7 +3,7 @@
 // TODO: Auto imports everything in assets
 // TODO: Prebuilt components coming from view
 
-$query='astolfo';
+$query='himegoto';
 exec("curl -Ls 'https://www.google.com/search?udm=2&q=$query' | pup 'img:first-of-type attr{src}' | tail -n +2", $images);
 
 view('index', ['theme' => 'blue', 'astolfo' => $images]);
